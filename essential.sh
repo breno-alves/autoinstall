d853f02c6ec00a533a097cb370db746c28687207
Breno Alves <breno550@hotmail.com>
2020-04-02 21:54:36 -0300

    fix yarn

--- essential.sh
+++ essential.sh
@@ -118,7 +118,9 @@ curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
 sudo apt-get -y -f install nodejs
 
 echo "installing yarn..."
-sudo apt-get -y -f install yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt-get -y -f install yarn
 
 echo "installing brave..."
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -

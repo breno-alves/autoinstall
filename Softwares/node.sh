086465208630a7dad73dc7b4b3b53ec19473b5c7
Breno Alves <breno550@hotmail.com>
2020-04-22 16:40:13 -0300

    more stuff

--- Softwares/node.sh
+++ Softwares/node.sh
@@ -4,3 +4,5 @@ sudo apt-get -y -f install nodejs
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update && sudo apt-get -y -f install yarn
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
